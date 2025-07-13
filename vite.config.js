bb1955c15d0ccbcaea2bfcbd20b14a90fd1eefbf
Azam Din Abdillah <azzamdinabdillah123@gmail.com>
2025-07-13 14:09:55 +0700

    fix: config tailwind and vite

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
-
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: 5000,
-    allowedHosts: ['c07afe90-d3b5-4483-9270-ddf7ec447368-00-1hmaup0ai5uy9.kirk.replit.dev', 'localhost', '127.0.0.1']
-  },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true
-  }
+  plugins: [
+    tailwindcss(),
+  ],
+  base: "./"
 })
